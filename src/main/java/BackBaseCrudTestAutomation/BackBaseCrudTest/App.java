a84b0c1e2ccb0d5737605129e10ca6ba938c6365
seazee1 <azbaba@hotmail.co.uk>
2019-02-23 17:03:34 +0100

    POM Test

---
+++ src/main/java/BackBaseCrudTestAutomation/BackBaseCrudTest/App.java
@@ -0,0 +1,13 @@
+package BackBaseCrudTestAutomation.BackBaseCrudTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
